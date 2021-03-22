ed0d45c1342a1e0e1e5d753944b1dc6dbc82d60e
Carmenprz <carmenp.gomez99@gmail.com>
2021-03-22 12:49:38 +0100

    DB: is_admin column to user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('points');
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
